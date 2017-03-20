289b8a2145b4987bcf6785c1ff98d352f2ed1c23
Jerrywx <23601145@qq.com>
2017-03-20 12:33:29 +0800

    提交代码

--- AttributeText-Demo/Classes/JRBlockController/header/jr_UICollectionViewLayout.h
+++ AttributeText-Demo/Classes/JRBlockController/header/jr_UICollectionViewLayout.h
@@ -84,18 +84,63 @@ typedef NS_ENUM(NSInteger, UICollectionUpdateAction) {
 /// UICollectionViewLayout
 @interface UICollectionViewLayout : NSObject <NSCoding>
 
-- (instancetype)init;
-
-- (nullable instancetype)initWithCoder:(NSCoder *)aDecoder;
-
+/* --------------------------------------------------------------------------- */
+/* Getting the Collection View Information */
+/* --------------------------------------------------------------------------- */
 
 // Methods in this class are meant to be overridden and will be called by its collection view to gather layout information.
 // To get the truth on the current state of the collection view, call methods on UICollectionView rather than these.
 @property (nullable, nonatomic, readonly) UICollectionView *collectionView;
 
+@property(nonatomic, readonly) CGSize collectionViewContentSize;
+// Subclasses must override this method and use it to return the width and height of the collection view’s content. These values represent the width and height of all the content, not just the content that is currently visible. The collection view uses this information to configure its own content size to facilitate scrolling.
+
+/* --------------------------------------------------------------------------- */
+/* Providing Layout Attributes */
+/* --------------------------------------------------------------------------- */
 // Call -invalidateLayout to indicate that the collection view needs to requery the layout information.
 // Subclasses must always call super if they override.
 - (void)invalidateLayout;
+
+// override this method to provide a custom class to be used when instantiating instances of UICollectionViewLayoutAttributes
+@property(class, nonatomic, readonly) Class layoutAttributesClass;
+
+// The collection view calls -prepareLayout once at its first layout as the first message to the layout instance.
+// The collection view calls -prepareLayout again after layout is invalidated and before requerying the layout information.
+// Subclasses should always call super if they override.
+- (void)prepareLayout;
+// UICollectionView calls these four methods to determine the layout information.
+// Implement -layoutAttributesForElementsInRect: to return layout attributes for for supplementary or decoration views, or to perform layout in an as-needed-on-screen fashion.
+// Additionally, all layout subclasses should implement -layoutAttributesForItemAtIndexPath: to return layout attributes instances on demand for specific index paths.
+// If the layout supports any supplementary or decoration view types, it should also implement the respective atIndexPath: methods for those types.
+- (nullable NSArray<__kindof UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect; // return an array layout attributes instances for all the views in the given rect
+- (nullable UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath;
+
+
+- (nullable UICollectionViewLayoutAttributes *)layoutAttributesForSupplementaryViewOfKind:(NSString *)elementKind atIndexPath:(NSIndexPath *)indexPath;
+
+- (nullable UICollectionViewLayoutAttributes *)layoutAttributesForDecorationViewOfKind:(NSString*)elementKind atIndexPath:(NSIndexPath *)indexPath;
+
+- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity; // return a point at which to rest after scrolling - for layouts that want snap-to-point scrolling behavior
+- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset NS_AVAILABLE_IOS(7_0); // a layout can return the content offset to be applied during transition or update animations
+
+
+/* --------------------------------------------------------------------------- */
+/* Responding to Collection View Updates */
+/* --------------------------------------------------------------------------- */
+// This method is called when there is an update with deletes/inserts to the collection view.
+// It will be called prior to calling the initial/final layout attribute methods below to give the layout an opportunity to do batch computations for the insertion and deletion layout attributes.
+// The updateItems parameter is an array of UICollectionViewUpdateItem instances for each element that is moving to a new index path.
+- (void)prepareForCollectionViewUpdates:(NSArray<UICollectionViewUpdateItem *> *)updateItems;
+- (void)finalizeCollectionViewUpdates; // called inside an animation block after the update
+- (NSArray<NSIndexPath *> *)indexPathsToInsertForSupplementaryViewOfKind:(NSString *)elementKind NS_AVAILABLE_IOS(7_0);
+- (NSArray<NSIndexPath *> *)indexPathsToInsertForDecorationViewOfKind:(NSString *)elementKind NS_AVAILABLE_IOS(7_0);
+
+
+
+- (instancetype)init;
+
+- (nullable instancetype)initWithCoder:(NSCoder *)aDecoder;
 
 - (void)invalidateLayoutWithContext:(UICollectionViewLayoutInvalidationContext *)context;
 
@@ -106,30 +151,14 @@ typedef NS_ENUM(NSInteger, UICollectionUpdateAction) {
 @end
 
 
+/// UICollectionViewLayout
 @interface UICollectionViewLayout (UISubclassingHooks)
 
-#if UIKIT_DEFINE_AS_PROPERTIES
-@property(class, nonatomic, readonly) Class layoutAttributesClass;
-// override this method to provide a custom class to be used when instantiating instances of UICollectionViewLayoutAttributes
+
+
+
 @property(class, nonatomic, readonly) Class invalidationContextClass NS_AVAILABLE_IOS(7_0); // override this method to provide a custom class to be used for invalidation contexts
-#else
-+ (Class)layoutAttributesClass; // override this method to provide a custom class to be used when instantiating instances of UICollectionViewLayoutAttributes
-+ (Class)invalidationContextClass NS_AVAILABLE_IOS(7_0); // override this method to provide a custom class to be used for invalidation contexts
-#endif
 
-// The collection view calls -prepareLayout once at its first layout as the first message to the layout instance.
-// The collection view calls -prepareLayout again after layout is invalidated and before requerying the layout information.
-// Subclasses should always call super if they override.
-- (void)prepareLayout;
-
-// UICollectionView calls these four methods to determine the layout information.
-// Implement -layoutAttributesForElementsInRect: to return layout attributes for for supplementary or decoration views, or to perform layout in an as-needed-on-screen fashion.
-// Additionally, all layout subclasses should implement -layoutAttributesForItemAtIndexPath: to return layout attributes instances on demand for specific index paths.
-// If the layout supports any supplementary or decoration view types, it should also implement the respective atIndexPath: methods for those types.
-- (nullable NSArray<__kindof UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect; // return an array layout attributes instances for all the views in the given rect
-- (nullable UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath;
-- (nullable UICollectionViewLayoutAttributes *)layoutAttributesForSupplementaryViewOfKind:(NSString *)elementKind atIndexPath:(NSIndexPath *)indexPath;
-- (nullable UICollectionViewLayoutAttributes *)layoutAttributesForDecorationViewOfKind:(NSString*)elementKind atIndexPath:(NSIndexPath *)indexPath;
 
 - (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds; // return YES to cause the collection view to requery the layout for geometry information
 - (UICollectionViewLayoutInvalidationContext *)invalidationContextForBoundsChange:(CGRect)newBounds NS_AVAILABLE_IOS(7_0);
@@ -137,24 +166,13 @@ typedef NS_ENUM(NSInteger, UICollectionUpdateAction) {
 - (BOOL)shouldInvalidateLayoutForPreferredLayoutAttributes:(UICollectionViewLayoutAttributes *)preferredAttributes withOriginalAttributes:(UICollectionViewLayoutAttributes *)originalAttributes NS_AVAILABLE_IOS(8_0);
 - (UICollectionViewLayoutInvalidationContext *)invalidationContextForPreferredLayoutAttributes:(UICollectionViewLayoutAttributes *)preferredAttributes withOriginalAttributes:(UICollectionViewLayoutAttributes *)originalAttributes NS_AVAILABLE_IOS(8_0);
 
-- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity; // return a point at which to rest after scrolling - for layouts that want snap-to-point scrolling behavior
-- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset NS_AVAILABLE_IOS(7_0); // a layout can return the content offset to be applied during transition or update animations
 
-#if UIKIT_DEFINE_AS_PROPERTIES
-@property(nonatomic, readonly) CGSize collectionViewContentSize; // Subclasses must override this method and use it to return the width and height of the collection view’s content. These values represent the width and height of all the content, not just the content that is currently visible. The collection view uses this information to configure its own content size to facilitate scrolling.
-#else
-- (CGSize)collectionViewContentSize; // Subclasses must override this method and use it to return the width and height of the collection view’s content. These values represent the width and height of all the content, not just the content that is currently visible. The collection view uses this information to configure its own content size to facilitate scrolling.
-#endif
 
 @end
 
 @interface UICollectionViewLayout (UIUpdateSupportHooks)
 
-// This method is called when there is an update with deletes/inserts to the collection view.
-// It will be called prior to calling the initial/final layout attribute methods below to give the layout an opportunity to do batch computations for the insertion and deletion layout attributes.
-// The updateItems parameter is an array of UICollectionViewUpdateItem instances for each element that is moving to a new index path.
-- (void)prepareForCollectionViewUpdates:(NSArray<UICollectionViewUpdateItem *> *)updateItems;
-- (void)finalizeCollectionViewUpdates; // called inside an animation block after the update
+
 
 - (void)prepareForAnimatedBoundsChange:(CGRect)oldBounds; // UICollectionView calls this when its bounds have changed inside an animation block before displaying cells in its new bounds
 - (void)finalizeAnimatedBoundsChange; // also called inside the animation block
@@ -179,17 +197,20 @@ typedef NS_ENUM(NSInteger, UICollectionUpdateAction) {
 // Return an array of index paths to indicate views that the layout is deleting or inserting in response to the update.
 - (NSArray<NSIndexPath *> *)indexPathsToDeleteForSupplementaryViewOfKind:(NSString *)elementKind NS_AVAILABLE_IOS(7_0);
 - (NSArray<NSIndexPath *> *)indexPathsToDeleteForDecorationViewOfKind:(NSString *)elementKind NS_AVAILABLE_IOS(7_0);
-- (NSArray<NSIndexPath *> *)indexPathsToInsertForSupplementaryViewOfKind:(NSString *)elementKind NS_AVAILABLE_IOS(7_0);
-- (NSArray<NSIndexPath *> *)indexPathsToInsertForDecorationViewOfKind:(NSString *)elementKind NS_AVAILABLE_IOS(7_0);
+
+
+
 
 @end
 
 @interface UICollectionViewLayout (UIReorderingSupportHooks)
 
 - (NSIndexPath *)targetIndexPathForInteractivelyMovingItem:(NSIndexPath *)previousIndexPath withPosition:(CGPoint)position NS_AVAILABLE_IOS(9_0);
+
 - (UICollectionViewLayoutAttributes *)layoutAttributesForInteractivelyMovingItemAtIndexPath:(NSIndexPath *)indexPath withTargetPosition:(CGPoint)position NS_AVAILABLE_IOS(9_0);
 
 - (UICollectionViewLayoutInvalidationContext *)invalidationContextForInteractivelyMovingItems:(NSArray<NSIndexPath *> *)targetIndexPaths withTargetPosition:(CGPoint)targetPosition previousIndexPaths:(NSArray<NSIndexPath *> *)previousIndexPaths previousPosition:(CGPoint)previousPosition NS_AVAILABLE_IOS(9_0);
+
 - (UICollectionViewLayoutInvalidationContext *)invalidationContextForEndingInteractiveMovementOfItemsToFinalIndexPaths:(NSArray<NSIndexPath *> *)indexPaths previousIndexPaths:(NSArray<NSIndexPath *> *)previousIndexPaths movementCancelled:(BOOL)movementCancelled NS_AVAILABLE_IOS(9_0);
 
 @end
