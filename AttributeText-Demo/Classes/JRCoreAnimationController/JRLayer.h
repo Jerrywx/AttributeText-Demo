afbb5f8272860fbfa5938cc1e694116b4e040a8f
Jerrywx <23601145@qq.com>
2017-04-12 20:29:49 +0800

    提交代码

---
+++ AttributeText-Demo/Classes/JRCoreAnimationController/JRLayer.h
@@ -0,0 +1,13 @@
+//
+//  JRLayer.h
+//  AttributeText-Demo
+//
+//  Created by 王潇 on 2017/4/3.
+//  Copyright © 2017年 王潇. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface JRLayer : CALayer
+
+@end
